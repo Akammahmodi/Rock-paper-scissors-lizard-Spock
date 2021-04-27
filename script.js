9b1316fee2dabb123d5c9e666fdca6a657d196f7
akammahmodi <akammahmodi88@gmail.com>
2021-04-27 20:54:28 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const player = 0
+const computer = 0
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("Rock");
+const paper_div = document.getElementById("Paper");
+const scissors_div = document.getElementById("Scissors");
+const lizard_div = document.getElementById("Lizard");
+const spock_div = document.getElementById("Spock");
 
+rock_div.addEventListener('click', function() {
+    console.log("hey you clicked on rock");
+});
